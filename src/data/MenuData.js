c9d5553e8969f66b68beb5521c809832053bd75f
Santiago Sosa <santisosalp@gmail.com>
2024-08-16 13:21:42 -0300

    navbar added

---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const menuData = [
+    {link:"/", content:"Inicio"},
+    {link:"/nosotros", content:"Nosotros"},
+    {link:"/productos", content:"Productos"},
+    {link:"/emprender", content:"Emprender"},
+    {link:"/donde-comprar", content:"Donde comprar"},
+]
